0e1361ab0e71de56ce538639202970ba4c698f84
laurasarasofia <laurasarasofiaa@gmail.com>
2022-04-01 20:34:52 +0300

    testailua
    wordlesanat xml ja json,
    tiedostoja yritetty lukee javalla ja pythonilla epäonnistuneesti

--- Wordle.java
+++ Wordle.java
@@ -1,3 +1,13 @@
+import java.io.*;
+import javax.xml.*;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Node;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.DocumentBuilder;
+
 /**
  * Wordle
  */
@@ -5,5 +15,12 @@ public class Wordle {
 
     public static void main(String[] args) {
         System.out.println("hello");
+        File myObj = new File("sanat.xml"); // Specify the filename
+        DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
+         DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
+         Document doc = dBuilder.parse(myObj);
+         doc.getDocumentElement().normalize();
+         System.out.println("Root element :" + doc.getDocumentElement().getNodeName());
+
     }
 }
