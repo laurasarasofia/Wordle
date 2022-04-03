4f1b2519d2b8ba692b2eb9bf1c73acc185f1de7c
laurasarasofia <laurasarasofiaa@gmail.com>
2022-04-03 18:41:31 +0300

    uusia luokkia SanojenKäsittely.java
    tulostaSana(), getSana() palauttaa String muotoisen sanan xml tiedostosta ja tiedostonKäsittely() palauttaa xml tiedsostonsanat muutettuna NodeList mutoon

--- SanojenKäsittely.java
+++ SanojenKäsittely.java
@@ -10,26 +10,45 @@ import org.w3c.dom.NodeList;
 
 public class SanojenKäsittely {
     File file;
+    NodeList lista;
 
     public SanojenKäsittely(File file){
         this.file=file;
     }
 
-    public void tulostaSana () throws SAXException, IOException, ParserConfigurationException{
-
+    public NodeList tiedostonKäsittely() throws ParserConfigurationException, SAXException, IOException{
+        
         DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance(); 
         DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
         Document doc = dBuilder.parse(file);
         doc.getDocumentElement().normalize();  //rivit 20-23 käsittelee sanat.xml tiedostoa käytettävään muotoon
 
         NodeList list = doc.getElementsByTagName("sana"); //muodostetaan NodeList lista sanat.xml sanoista
-        int min = 0; 
-        int max =list.getLength(); //kertoo sanojen määrän
-    
         System.out.println("Root element :" + doc.getDocumentElement().getNodeName()); //Tulostaa juuren nimen (ei tarpeellinen toiminnan kannalta)
+        this.lista=list;
+        return list;
+    }
 
+    public void tulostaSana () throws SAXException, IOException, ParserConfigurationException{
+
+        NodeList lista = tiedostonKäsittely(); //kutsuu tiedostonKäsittely luokkaa saadakseen sanalistan NodeList muodossa
+
+        int min = 0; 
+        int max =lista.getLength(); //kertoo sanojen määrän
         int b = (int)(Math.random()*(max-min+1)+min);  //arpoo random numeron, jota käytetään sanalistasta random sanan valintaan
-        System.out.println(doc.getElementsByTagName("sana").item(b).getTextContent()); //tulostaa sanalistasta random sanan
+        System.out.println(lista.item(b).getTextContent());
+    }
+
+    public String getSana () throws ParserConfigurationException, SAXException, IOException{
+        
+        NodeList lista = tiedostonKäsittely(); //kutsuu tiedostonKäsittely luokkaa saadakseen sanalistan NodeList muodossa
+
+        int min = 0; 
+        int max =lista.getLength(); //kertoo sanojen määrän
+        int b = (int)(Math.random()*(max-min+1)+min);  //arpoo random numeron, jota käytetään sanalistasta random sanan valintaan
+        String sana = lista.item(b).getTextContent();
+        
+        return sana;
     }
 
 }
