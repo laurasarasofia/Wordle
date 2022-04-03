996d54c3c6cf5500b6ae544582b3abca910eb54c
laurasarasofia <laurasarasofiaa@gmail.com>
2022-04-03 18:17:19 +0300

    main muokattu järkevämmäks
    poistettu importtei, jotka on SanojenKäsittely.java

--- Wordle.java
+++ Wordle.java
@@ -1,20 +1,15 @@
 import java.io.*;
-import javax.xml.*;
-import org.w3c.dom.Document;
-import org.w3c.dom.Element;
-import org.w3c.dom.Node;
-import org.w3c.dom.NodeList;
-import org.xml.sax.SAXException;
-import javax.xml.parsers.DocumentBuilderFactory;
+
 import javax.xml.parsers.ParserConfigurationException;
-import javax.xml.parsers.DocumentBuilder;
+
+import org.xml.sax.SAXException;
 
 /**
  * Wordle
  */
 public class Wordle {
 
-    public static void main(String[] args) throws ParserConfigurationException, SAXException, IOException {
+    public static void main(String[] args) throws SAXException, IOException, ParserConfigurationException  {
 
         System.out.println("hello");
         File myObj = new File("sanat.xml"); // Specify the filename
